997d0acbc80d5a410d85df659a36a7d341c9944c
Bruno Lessa <brunolessa@live.com>
2023-03-25 20:03:39 -0300

    root layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
 import Description from "./components/Description";
 import Header from "./components/Header";
 import Images from "./components/Images";
@@ -11,24 +10,21 @@ import Title from "./components/Title";
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white text-black w-[70%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Title />
-            <MyRating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
+    <>
+      <Header />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white text-black w-[70%] rounded p-3 shadow">
+          <RestaurantNavbar />
+          <Title />
+          <MyRating />
+          <Description />
+          <Images />
+          <Reviews />
         </div>
-      </main>
-    </main>
+        <div className="w-[27%] relative text-reg">
+          <ReservationCard />
+        </div>
+      </div>
+    </>
   );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import Sidebar from "./components/Sidebar";
 
 export default function Search() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          <Sidebar />
-          <div className="w-5/6">
-            <RestaurantCard />
-          </div>
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <Sidebar />
+        <div className="w-5/6">
+          <RestaurantCard />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 }
