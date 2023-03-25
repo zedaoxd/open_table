683ffb02239a155e8440556670cb3c6eabd4d092
Bruno Lessa <brunolessa@live.com>
2023-03-25 20:10:39 -0300

    Nested Components

---
+++ app/restaurant/[slug]/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+
+export default function RestaurantLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        {children}
+      </div>
+    </>
+  );
+}
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -5,14 +5,9 @@ import RestaurantNavbar from "../components/RestaurantNavbar";
 
 export default function RestaurantMenu() {
   return (
-    <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavbar />
-          <Menu />
-        </div>
-      </div>
-    </>
+    <div className="bg-white w-[100%] rounded p-3 shadow">
+      <RestaurantNavbar />
+      <Menu />
+    </div>
   );
 }
