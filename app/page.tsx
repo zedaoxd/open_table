eb790464daa4ec8f8fc4a9c8639e6bbc8c1a4bad
Bruno Lessa <brunolessa@live.com>
2023-03-25 23:52:15 -0300

    fecthRestaurants

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const Prisma = new PrismaClient();
+
+const fetchRestaurants = async () => {
+  const restaurants = await Prisma.restaurant.findMany();
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+  console.log({ restaurants });
   return (
     <main>
       <Header />
