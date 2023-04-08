f6f811bffa6e00c606862598fb89b8f07842379d
Bruno Lessa <brunolessa@live.com>
2023-04-08 01:20:48 -0300

    Determining the search times

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { times } from "../../../../data";
 
 type QueryParams = {
   slug: string;
@@ -11,13 +12,23 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
   const { slug, day, time, partySize } = req.query as QueryParams;
 
   if (!day || !time || !partySize) {
-    return res.status(422).json({ errors: ["Missing query params"] });
+    return res.status(422).json({ error: "Missing query params" });
   }
 
-  return res.status(200).json({ slug, day, time, partySize });
+  const searchTimes = times.find((t) => t.time === time);
+
+  if (!searchTimes) {
+    return res.status(422).json({ error: "Invalid time" });
+  }
+
+  return res.status(200).json(searchTimes);
 }
 
 // http://localhost:3000/api/restaurant/coconut-lagoon-ottawa/availability
