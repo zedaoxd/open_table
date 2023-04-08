b72071e5e11957233036d9b9baa8b2c4f93d11fc
Bruno Lessa <brunolessa@live.com>
2023-04-07 21:06:49 -0300

    Redering a datePicker component

--- app/restaurant/[slug]/components/ReservationCard/index.tsx
+++ app/restaurant/[slug]/components/ReservationCard/index.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { partySize } from "../../../../../data";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function ReservationCard() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+  const handleChangeDate = (date: Date | null) => {
+    if (date) return setSelectedDate(date);
+    return setSelectedDate(null);
+  };
+
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
       <div className="text-center border-b pb-2 font-bold">
@@ -21,7 +31,13 @@ export default function ReservationCard() {
       <div className="flex justify-between">
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Date</label>
-          <input type="text" className="py-3 border-b font-light w-28" />
+          <DatePicker
+            selected={selectedDate}
+            onChange={handleChangeDate}
+            className="py-3 border-b font-light text-reg w-[100%]"
+            dateFormat="MMMM d"
+            wrapperClassName="w-[48]"
+          />
         </div>
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
