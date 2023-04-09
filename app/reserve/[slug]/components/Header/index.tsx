98cdd5183aa8fe89e0cb162c83c3f5d16d27a088
Bruno Lessa <brunolessa@live.com>
2023-04-09 19:16:42 -0300

    Extracting the query params and redenring data for the reserve header

--- app/reserve/[slug]/components/Header/index.tsx
+++ app/reserve/[slug]/components/Header/index.tsx
@@ -1,21 +1,32 @@
 import React from "react";
+import {
+  Time,
+  convertToDisplayTime,
+} from "../../../../../utils/convertToDisplayTime";
+import { format } from "date-fns";
 
-export default function Header() {
+type Props = {
+  image: string;
+  name: string;
+  date: string;
+  partySize: string;
+};
+
+export default function Header({ image, name, date, partySize }: Props) {
+  const [day, time] = date.split("T");
   return (
     <div>
       <h3 className="font-bold">You're almost done!</h3>
       <div className="mt-5 flex">
-        <img
-          src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-          alt=""
-          className="w-32 h-18 rounded"
-        />
+        <img src={image} alt={name} className="w-32 h-18 rounded" />
         <div className="ml-4">
-          <h1 className="text-3xl font-bold">Aiāna Restaurant Collective</h1>
+          <h1 className="text-3xl font-bold">{name}</h1>
           <div className="flex mt-3">
-            <p className="mr-6">Tues, 22, 2023</p>
-            <p className="mr-6">7:30 PM</p>
-            <p className="mr-6">3 people</p>
+            <p className="mr-6">{format(new Date(date), "ccc, LLL d")}</p>
+            <p className="mr-6">{convertToDisplayTime(time as Time)}</p>
+            <p className="mr-6">
+              {partySize} {Number(partySize) > 1 ? "people" : "person"}
+            </p>
           </div>
         </div>
       </div>
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,12 +1,45 @@
 import React from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
-export default function Reserve() {
+const prisma = new PrismaClient();
+
+const fecthRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+  return restaurant;
+};
+
+type Props = {
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    date: string;
+    partySize: string;
+  };
+};
+
+export default async function Reserve({ params, searchParams }: Props) {
+  const restaurant = await fecthRestaurantBySlug(params.slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         <Form />
       </div>
     </div>
