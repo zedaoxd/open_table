93aaf279892b345961f0ff63e5612b0b22b31b15
Bruno Lessa <brunolessa@live.com>
2023-04-09 02:24:42 -0300

    Validation time

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,4 +1,7 @@
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
 
 type QueryParams = {
   slug: string;
@@ -7,10 +10,28 @@ type QueryParams = {
   partySize: string;
 };
 
-// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2021-08-01&time=18:00&numberOfPerple=2
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2021-08-01&time=18:00&partySize=2
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { slug, day, time, partySize } = req.query as QueryParams;
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+  });
+
+  if (!restaurant) {
+    return res.status(404).json({ error: "Restaurant not found" });
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+  ) {
+    return res.status(422).json({ error: "Invalid time" });
+  }
 
   res.status(200).json({ slug, day, time, partySize });
 }
